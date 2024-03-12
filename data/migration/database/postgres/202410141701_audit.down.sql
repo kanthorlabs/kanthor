448c69c5c6bdb94ad4dac23804a6df8d86df0fd3
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-03-12 09:44:27 +0700

    feat: update audit migration script

--- data/migration/database/postgres/202410141701_audit.down.sql
+++ data/migration/database/postgres/202410141701_audit.down.sql
@@ -2,8 +2,14 @@ BEGIN;
 
 DROP TRIGGER IF EXISTS kanthor_audit_trigger_log_workspace ON public.kanthor_workspace;
 
-DROP FUNCTION IF EXISTS kanthor_audit_trigger;
+DROP TRIGGER IF EXISTS kanthor_audit_trigger_log_application ON public.kanthor_application;
+
+DROP TRIGGER IF EXISTS kanthor_audit_trigger_log_endpoint ON public.kanthor_endpoint;
+
+DROP TRIGGER IF EXISTS kanthor_audit_trigger_log_endpoint_rule ON public.kanthor_endpoint_rule;
 
 DROP TABLE IF EXISTS kanthor_audit;
+
+DROP FUNCTION IF EXISTS kanthor_audit_trigger;
  
 COMMIT;
--- data/migration/database/postgres/202410141701_audit.up.sql
+++ data/migration/database/postgres/202410141701_audit.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS kanthor_audit (
   operator VARCHAR(256),
   rand VARCHAR(36) DEFAULT gen_random_uuid(),
   PRIMARY KEY (table_name, id, operation, operator, rand),
-  created_at TIMESTAMP DEFAULT now(),
+  created_at BIGINT	DEFAULT ROUND(EXTRACT(EPOCH FROM NOW()) * 1000),
   previous jsonb,
   current jsonb
 );
@@ -16,13 +16,15 @@ CREATE OR REPLACE FUNCTION kanthor_audit_trigger() RETURNS TRIGGER AS $$
 DECLARE
     new_data jsonb;
     old_data jsonb;
-    key text;
+    diffkey text;
     new_values jsonb;
     old_values jsonb;
     operator VARCHAR(256);
+    ts BIGINT;
 BEGIN
     new_values := '{}';
     old_values := '{}';
+    ts := ROUND(EXTRACT(EPOCH FROM NOW()) * 1000);
 
     IF TG_OP = 'INSERT' THEN
         new_data := to_jsonb(NEW);
@@ -34,11 +36,11 @@ BEGIN
         old_data := to_jsonb(OLD);
         operator := NEW.modifier;
 
-        FOR key IN SELECT jsonb_object_keys(new_data) INTERSECT SELECT jsonb_object_keys(old_data)
+        FOR diffkey IN SELECT jsonb_object_keys(new_data) INTERSECT SELECT jsonb_object_keys(old_data)
         LOOP
-            IF new_data ->> key != old_data ->> key THEN
-                new_values := new_values || jsonb_build_object(key, new_data ->> key);
-                old_values := old_values || jsonb_build_object(key, old_data ->> key);
+            IF new_data ->> diffkey != old_data ->> diffkey THEN
+                new_values := new_values || jsonb_build_object(diffkey, new_data ->> diffkey);
+                old_values := old_values || jsonb_build_object(diffkey, old_data ->> diffkey);
             END IF;
         END LOOP;
 
@@ -46,44 +48,45 @@ BEGIN
         old_data := to_jsonb(OLD);
         old_values := old_data;
         operator := OLD.modifier;
+        ts := ts + 1;
 
-        FOR key IN SELECT jsonb_object_keys(old_data)
+        FOR diffkey IN SELECT jsonb_object_keys(old_data)
         LOOP
-            old_values := old_values || jsonb_build_object(key, old_data ->> key);
+            old_values := old_values || jsonb_build_object(diffkey, old_data ->> diffkey);
         END LOOP;
 
     END IF;
 
     IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
         INSERT INTO kanthor_audit (table_name, id, operation, operator, created_at, previous, current)
-        VALUES (TG_TABLE_NAME, NEW.id, TG_OP, operator, now(), old_values, new_values);
+        VALUES (TG_TABLE_NAME, NEW.id, TG_OP, operator, ts, old_values, new_values);
 
         RETURN NEW;
     ELSE
         INSERT INTO kanthor_audit (table_name, id, operation, operator, created_at, previous, current)
-        VALUES (TG_TABLE_NAME, OLD.id, TG_OP, operator, now(), old_values, new_values);
+        VALUES (TG_TABLE_NAME, OLD.id, TG_OP, operator, ts, old_values, new_values);
 
         RETURN OLD;
     END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER kanthor_audit_trigger_log_workspace BEFORE INSERT OR UPDATE OR DELETE 
+CREATE OR REPLACE TRIGGER kanthor_audit_trigger_log_workspace BEFORE INSERT OR UPDATE OR DELETE 
 ON public.kanthor_workspace
 FOR EACH ROW
 EXECUTE FUNCTION kanthor_audit_trigger(); 
 
-CREATE TRIGGER kanthor_audit_trigger_log_application BEFORE INSERT OR UPDATE OR DELETE 
+CREATE OR REPLACE TRIGGER kanthor_audit_trigger_log_application BEFORE INSERT OR UPDATE OR DELETE 
 ON public.kanthor_application
 FOR EACH ROW
 EXECUTE FUNCTION kanthor_audit_trigger(); 
 
-CREATE TRIGGER kanthor_audit_trigger_log_endpoint BEFORE INSERT OR UPDATE OR DELETE 
+CREATE OR REPLACE TRIGGER kanthor_audit_trigger_log_endpoint BEFORE INSERT OR UPDATE OR DELETE 
 ON public.kanthor_endpoint
 FOR EACH ROW
 EXECUTE FUNCTION kanthor_audit_trigger(); 
 
-CREATE TRIGGER kanthor_audit_trigger_log_endpoint_rule BEFORE INSERT OR UPDATE OR DELETE 
+CREATE OR REPLACE TRIGGER kanthor_audit_trigger_log_endpoint_rule BEFORE INSERT OR UPDATE OR DELETE 
 ON public.kanthor_endpoint_rule
 FOR EACH ROW
 EXECUTE FUNCTION kanthor_audit_trigger(); 
