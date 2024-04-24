b1419a3cd8b252222d68d7b020278328e7fb0234
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-04-24 09:58:11 +0700

    feat: add otelcol

--- scripts/localdev.sh
+++ scripts/localdev.sh
@@ -2,16 +2,25 @@
 set -e
 
 SHOULD_BUILD=${SHOULD_BUILD:-""}
+SHOULD_MONITOR=${SHOULD_MONITOR:-""}
 MODE=${MODE:-"START"}
 
 if [ "$MODE" != "START" ];
 then
   docker compose -f docker-compose.yaml -f docker-compose.localdev.yaml stop storage dispatcher scheduler sdk portal startup
   docker compose -f docker-compose.yaml -f docker-compose.localdev.yaml rm -f storage dispatcher scheduler sdk portal startup
+  
+  docker compose -f docker-compose.monitoring.yaml stop otelcol 
+  docker compose -f docker-compose.monitoring.yaml rm -f otelcol 
   exit 0
 fi
 
 make generator migrate-up
+
+if [ "$SHOULD_BUILD" != "" ];
+then
+  docker compose -f docker-compose.yaml -f docker-compose.monitoring.yaml up -d otelcol 
+fi
 
 if [ "$SHOULD_BUILD" != "" ];
 then
